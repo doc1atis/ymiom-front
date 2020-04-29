24ed70a278ff8f668ea731dd338f4e89a07d37bc
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-04-29 17:16:30 -0400

    fixed api route

--- src/API/api.js
+++ src/API/api.js
@@ -1,9 +1,7 @@
 import Axios from "axios";
+// "http://localhost:3001/api"
 export default Axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3001/api"
-      : "/api",
+  baseURL: process.env.NODE_ENV === "development" ? "/api" : "/api",
   headers: {
     "authorization-x-token": localStorage.getItem("jwtToken"),
     // "content-type": "multipart/form-data"
