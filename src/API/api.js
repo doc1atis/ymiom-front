497ecc27459f019a2afcb7858dcb4a463a147f98
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-04-29 17:40:42 -0400

    dev env added

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,10 @@
 import Axios from "axios";
 // "http://localhost:3001/api"
 export default Axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "/api" : "/api",
+  baseURL:
+    process.env.REACT_APP_APP_PHASE === "development"
+      ? "http://localhost:3001/api"
+      : "/api",
   headers: {
     "authorization-x-token": localStorage.getItem("jwtToken"),
     // "content-type": "multipart/form-data"
